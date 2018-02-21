cf8cca33ec0d2991d19b126d608302f780aca59d
EC2 Default User <ec2-user@ip-172-31-23-213.us-east-2.compute.internal>
2018-02-21 18:19:56 +0000

    CRUD messages

---
+++ resources/views/messages/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+    <h1>メッセージ新規作成ページ</h1>
+
+    {!! Form::model($message, ['route' => 'messages.store']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}        
+
+@endsection
---
+++ resources/views/messages/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+    <h1>id = {{ $message->id }} のメッセージ詳細ページ</h1>
+
+    <p>{{ $message->content }}</p>
+
+    {!! link_to_route('messages.edit', 'このメッセージ編集', ['id' => $message->id]) !!}
+    
+    {!! Form::model($message, ['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+    
+@endsection
